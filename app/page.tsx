8ce989ded94c18b8f69abed247213a6b927b9a1f
三島 <k.mishima@fukuyamaconsul.co.jp>
2025-09-11 21:23:46 +0000

    init

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <a href="https://projectschool.dev/exercises/reactlessons">Project School React</a>
+      </header>
+      <section>
+        <header>React</header>
+        <div>
+          <div>
+            <p>Lesson 1</p>
+            <p>React 3D Interactive Card</p>
+            <p>このレッスンでは、Reactの関数コンポーネントを使ってカード構造を作ります。マウス移動のハンドリングやenter, leaveのイベントリスナーによる視覚効果の作成やインタラクションをコンポーネントに実装します。</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
