c1af1186b636a57badaede8ce6833161d9bdcf6a
三島 <ilfr1616@gmail.com>
2025-10-01 15:52:55 +0900

    add: lesson6 (#16)

---
+++ app/lesson6/data.ts
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "Why is React so awesome?",
+    info: "React is amazing because it allows you to build dynamic and interactive user interfaces with ease. Its component-based architecture makes it easy to reuse code and maintain your application. Plus, its virtual DOM implementation ensures fast rendering performance, making React a top choice for frontend development.",
+  },
+  {
+    id: 2,
+    title: "How can React enhance my web development experience?",
+    info: "React simplifies the process of building complex UIs by breaking them down into reusable components. Its declarative syntax makes it straightforward to understand and modify your code. With React, you can efficiently manage state and handle user interactions, resulting in a more enjoyable and productive development experience.",
+  },
+  {
+    id: 3,
+    title: "What are the key advantages of using React?",
+    info: "React offers numerous benefits, including improved performance, code reusability, and a vibrant ecosystem of libraries and tools. Its virtual DOM enables efficient updates to the UI, reducing unnecessary re-renders and enhancing overall application performance. Additionally, React's component-based architecture promotes modularity and scalability, making it easier to maintain and extend your codebase over time.",
+  },
+  {
+    id: 4,
+    title: "How does React revolutionize frontend development?",
+    info: "React revolutionizes frontend development by introducing a more efficient and scalable approach to building user interfaces. Its component-based architecture encourages code reuse and separation of concerns, leading to cleaner and more maintainable code. Furthermore, React's unidirectional data flow and virtual DOM enable high-performance rendering and seamless updates to the UI, resulting in a smoother user experience.",
+  },
+  {
+    id: 5,
+    title: "What makes React the go-to choice for modern web development?",
+    info: "React's simplicity, performance, and strong community support make it the preferred framework for modern web development. Its component-based architecture and declarative syntax streamline the development process, allowing developers to focus on building great user experiences. Additionally, React's ecosystem offers a wealth of third-party libraries and tools to enhance productivity and extend functionality. Overall, React empowers developers to create sophisticated web applications with speed, efficiency, and ease.",
+  },
+];
+export default questions;
